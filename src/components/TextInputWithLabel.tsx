2864a6d56b8ea13d5270a7796ea22cb4aa3f3f91
jstaylor926 <54418577+jstaylor926@users.noreply.github.com>
2022-02-11 08:58:48 -0500

    input with label

---
+++ src/components/TextInputWithLabel.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+interface TextInputWithLabelProps {
+    id: any;
+    onInputChange: any;
+    value: any;
+    type: any;
+    isFocused: any;
+    children: any;
+
+}
+const TextInputWithLabel = ({id, onInputChange, value, type = 'text', isFocused, children}:TextInputWithLabelProps) => {
+    const inputRef = React.useRef();
+
+    React.useEffect(() => {
+        if (isFocused && inputRef.current) {
+            // @ts-ignore
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
+
+    return (
+        <>
+            <label htmlFor={id}>{children}</label>
+            &nbsp;
+            <input
+                id={id}
+                // @ts-ignore
+                ref={inputRef}
+                type={type}
+                value={value}
+                onChange={onInputChange}
+            />
+        </>
+    );
+}
+
+export default TextInputWithLabel;
